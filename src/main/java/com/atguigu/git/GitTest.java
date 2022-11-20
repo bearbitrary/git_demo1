3ab21db32b3484d79511f2007acab9aa04325890
root <root@123456.com>
2022-11-20 10:02:20 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author ----DY
+ * @create 2022-11-20-9:54
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
